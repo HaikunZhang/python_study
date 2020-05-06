2d953a72ee22fdfe66b80ac3844f6196e3025ef4
haikun <haikun.zhang@foxmail.com>
2020-05-06 23:44:16 +0800

    nothing

--- python3.6_samples/study1.py
+++ python3.6_samples/study1.py
@@ -13,3 +13,23 @@ print('''这是第一行
 
 # 3. 不转义输出
 print(r"\n\\\\-->加r表示不转义")
+
+
+print(str(1234) + str(5678))
+
+print(repr(1234) + repr(5678))
+
+
+
+
+b1 = b'abc'
+print(b1)
+b2='我爱你'.encode()
+print(b2)
+b3=bytes("我爱你","UTF-8")
+print(b3)
+b4 = b'\xe6\x88\x91\xe7\x88\xb1\xe4\xbd\xa0'
+print(b4.decode('UTF-8'))
+
+
+a, b , c =range(5)
